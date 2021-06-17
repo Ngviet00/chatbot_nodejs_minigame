5a5a582b2bd529d7df59faf3f477ceb66cacd8b6
ngviet00 <ngvviet0306@gmail.com>
2021-06-17 22:14:38 +0700

    update

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -4,29 +4,55 @@ const MY_VERIFY_TOKEN = process.env.MY_VERIFY_TOKEN;
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 var User = require('./../DB/User');
 
+//co
 let getSpinWheel = (req, res) => {
-   // User.find()
-   //    .then((result) => {
-   //       const myDoc = result;
-   //       return res.render("spinwheel.ejs", { myDoc: myDoc });
-   //    })
-   //    .catch((err) => { console.log(err) })
    return res.render("spinwheel.ejs");
 }
+//co
 let postSpinWheel = async (req, res) => {
-   // let response = {
-   //    "text": `Chúc mừng em đã nhận được ${req.body.display_value_spin} khi trúng tuyển vào trường! Nhà trường sẽ liên hệ lại tư vấn thêm cho em và lưu lại thông tin học bổng của em nhé!`
-   // };
-   // callSendAPI(req.body.psid, response);
-   // await User.update(
-   //    { psid: req.body.psid },
-   //    { $set: { prize: req.body.display_value_spin, checkPrize: 1 } });
-   // console.log('updated');
-   // return res.redirect("/");
+
    let response = {
-      "text": `Chúc mừng em đã nhận được ${req.body.display_value_spin} khi trúng tuyển vào trường, chúc em sớm trở thành tân sinh viên của Trường Đại học Kinh Bắc nhé!`
+      "text": `Chúc mừng em đã nhận được ${req.body.display_value_spin} khi trúng tuyển vào trường! Nhà trường sẽ liên hệ lại tư vấn thêm cho em và lưu lại thông tin học bổng của em nhé!`
    };
-   let register = {
+   callSendAPI(req.body.psid, response);
+
+   await User.updateMany({ psid: req.body.psid }, {
+      $set: {
+         prize: req.body.display_value_spin,
+         checkPrize: 1
+      }
+   });
+   console.log("update thanh cong");
+   return res.redirect("/");
+}
+//co
+let getWebViewRegister2 = (req, res) => {
+   return res.render("register2.ejs");
+}
+//co
+let postWebViewRegister2 = (req, res) => {
+
+   let response = {
+      "text": `Bạn đã đăng ký thành công,Bộ phận tuyển sinh của Phòng Đào Tạo sẽ liên hệ lại với em, em nhớ để ý điện thoại em nhé!Chúc em sớm trở thành Sinh viên của Trường Đại Học Kinh Bắc!`
+   };
+   callSendAPI(req.body.psid, response);
+   var newUser = new User();
+   newUser.psid = req.body.psid;
+   newUser.name = req.body.name;
+   newUser.number = req.body.number;
+   newUser.email = req.body.email;
+   newUser.txtDate = req.body.txtDate;
+   newUser.major = req.body.major;
+   newUser.address = req.body.address;
+   newUser.prize = "";
+   newUser.checkPrize = 0;
+   newUser.save().then(function (err) {
+      if (err) { console.log(err) }
+      else {
+         console.log("them thanh cong");
+      }
+   });
+   let prize = {
       "attachment": {
          "type": "template",
          "payload": {
@@ -35,8 +61,8 @@ let postSpinWheel = async (req, res) => {
             "buttons": [
                {
                   "type": "web_url",
-                  "url": "https://webview-chatbot.herokuapp.com/register",
-                  "title": "ĐĂNG KÝ ",
+                  "url": "https://webview-chatbot.herokuapp.com/spin",
+                  "title": "Quay thưởng",
                   "messenger_extensions": "true",
                   "webview_height_ratio": "tall"
                }
@@ -44,30 +70,17 @@ let postSpinWheel = async (req, res) => {
          }
       }
    }
-   callSendAPI(req.body.psid, response);
-   callSendAPI(req.body.psid, register);
-   var newUser = new User();
-   newUser.psid = req.body.psid;
-   newUser.name = "";
-   newUser.number = "";
-   newUser.email = "";
-   newUser.txtDate = "";
-   newUser.major = "";
-   newUser.address = "";
-   newUser.prize = req.body.display_value_spin;
-   newUser.checkPrize = 1;
-   newUser.save().then(function (err) {
-      if (err) { console.log(err) }
-      else {
-         console.log("them thanh cong");
-      }
-   });
+   callSendAPI(req.body.psid, prize);
+   console.log('update thanh cong');
    return res.redirect("/");
 }
+
+//true
 let getSpinWheel2 = (req, res) => {
    return res.render("spinwheel2.ejs");
 }
-let postSpinWheel2 = async (req, res) => {
+//true
+let postSpinWheel2 = (req, res) => {
    let response = {
       "text": `Chúc mừng em đã nhận được ${req.body.display_value_spin} khi trúng tuyển vào trường, chúc em sớm trở thành tân sinh viên của Trường Đại học Kinh Bắc nhé!`
    };
@@ -109,9 +122,11 @@ let postSpinWheel2 = async (req, res) => {
    });
    return res.redirect("/");
 }
+//true
 let getWebViewRegister = (req, res) => {
    return res.render("register.ejs");
 }
+//true
 let postWebViewRegister = async (req, res) => {
 
    let response = {
@@ -132,29 +147,7 @@ let postWebViewRegister = async (req, res) => {
    console.log('update thanh cong');
    return res.redirect("/");
 }
-let getWebViewRegister2 = (req, res) => {
-   return res.render("register.ejs");
-}
-let postWebViewRegister2 = async (req, res) => {
 
-   let response = {
-      "text": `Bạn đã đăng ký thành công,Bộ phận tuyển sinh của Phòng Đào Tạo sẽ liên hệ lại với em, em nhớ để ý điện thoại em nhé!Chúc em sớm trở thành Sinh viên của Trường Đại Học Kinh Bắc!`
-   };
-   callSendAPI(req.body.psid, response);
-
-   await User.updateMany({ psid: req.body.psid }, {
-      $set: {
-         name: req.body.name,
-         number: req.body.number,
-         email: req.body.email,
-         txtDate: req.body.txtDate,
-         major: req.body.major,
-         address: req.body.address
-      }
-   });
-   console.log('update thanh cong');
-   return res.redirect("/");
-}
 let getHomepage = (req, res) => {
 
    return res.render("homepage.ejs");
